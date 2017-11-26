a7ac1fa7c8a5deaa435e779701edb1278271ceea
Bruno Stark <brunostark2003@gmail.com>
2017-11-26 15:32:34 -0800

    Add files via upload

---
+++ AutonomousTeamRed.java
@@ -0,0 +1,78 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+@Autonomous
+public class AutonomousTeamRed extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor Motor1 = null;
+    private DcMotor Motor2 = null;
+    private DcMotor Motor3 = null;
+    private DcMotor Motor4 = null;
+    private ColorSensor colorSensor;
+    
+    @Override
+    public void runOpMode () {
+        boolean bLedOn = true;
+        boolean isOpModeActive = true;
+        
+        //Ititialization
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+    
+        Motor1 = hardwareMap.get(DcMotor.class, "Motor1");
+        Motor2 = hardwareMap.get(DcMotor.class, "Motor2");
+        Motor3 = hardwareMap.get(DcMotor.class, "Motor3");
+        Motor4 = hardwareMap.get(DcMotor.class, "Motor4");
+        colorSensor = hardwareMap.get(ColorSensor.class, "ColorSensor1");
+        
+        Motor1.setDirection(DcMotor.Direction.FORWARD);
+        Motor2.setDirection(DcMotor.Direction.REVERSE);
+        Motor3.setDirection(DcMotor.Direction.REVERSE);
+        Motor4.setDirection(DcMotor.Direction.FORWARD);
+        colorSensor.enableLed(bLedOn);
+       
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+        
+        // run until the end of the match driver presses stop or end of 30 seconds
+        while ((runtime.seconds() <= 30)&&(opModeIsActive())&&(isOpModeActive == true)) {
+        
+            //Drive robot forward
+            Motor1.setPower(1);
+            Motor2.setPower(1);
+            Motor3.setPower(1);
+            Motor4.setPower(1);
+            
+            //Check for team color tape
+            if(colorSensor.red()>1&&colorSensor.blue()==0){
+                telemetry.addData("red", colorSensor.red());
+                telemetry.update();
+                isOpModeActive = false;
+            }
+            
+            telemetry.addData("Status", "Run Time: " + (int) runtime.seconds());
+            telemetry.update();
+        }
+        //stop motors
+        Motor1.setPower(0);
+        Motor2.setPower(0);
+        Motor3.setPower(0);
+        Motor4.setPower(0);
+        
+        
+    }
+}
---
+++ BrunoEverythingV3.java
@@ -0,0 +1,141 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@TeleOp(name="Bruno: Everything-V1", group="Linear Opmode")
+
+public class BrunoEverythingV1 extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private Servo leftGrip      = null;
+    private Servo rightGrip     = null;
+    private DcMotor Extend      = null;
+    private DcMotor Chain       = null;
+    private DcMotor Motor1      = null;
+    //private DcMotor Motor2      = null;
+    //private DcMotor Motor3      = null;
+    private DcMotor Motor4      = null;
+    private double left_pos     = 0.5;
+    private double right_pos    = 0.5;
+    static final double MAX_POS = 1.0;
+    static final double MIN_POS = 0.0;
+    private double ExtendPower;
+    private double ChainPower;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+    
+    //Hardware Map    
+    leftGrip  = hardwareMap.get(Servo.class, "left_servo");
+    rightGrip = hardwareMap.get(Servo.class, "right_servo");
+    Extend    = hardwareMap.get(DcMotor.class, "MotorExtend");
+    Chain     = hardwareMap.get(DcMotor.class, "MotorLift");
+    Motor1    = hardwareMap.get(DcMotor.class, "Motor1");
+    //Motor2    = hardwareMap.get(DcMotor.class, "Motor2");
+    //Motor3    = hardwareMap.get(DcMotor.class, "Motor3");
+    Motor4    = hardwareMap.get(DcMotor.class, "Motor4");
+    
+    //Setup
+    leftGrip.scaleRange(MIN_POS,MAX_POS);
+    rightGrip.scaleRange(MIN_POS,MAX_POS);
+    leftGrip.setPosition(left_pos);
+    rightGrip.setPosition(right_pos);
+    Extend.setDirection(DcMotor.Direction.FORWARD);
+    Chain.setDirection(DcMotor.Direction.FORWARD);
+    Motor1.setDirection(DcMotor.Direction.FORWARD);
+    //Motor2.setDirection(DcMotor.Direction.FORWARD);
+    //Motor3.setDirection(DcMotor.Direction.FORWARD);
+    Motor4.setDirection(DcMotor.Direction.FORWARD);
+        
+        // Wait for start
+        waitForStart();
+        runtime.reset();
+        
+        //Main
+        while (opModeIsActive()) {
+        
+        //Grabber
+        if (gamepad2.dpad_down){
+            left_pos +=0.05;
+            right_pos -=0.05;
+            }
+        if (gamepad2.dpad_up){
+            left_pos -=0.05;
+            right_pos +=0.05;
+            }
+        if (right_pos >=1.0){
+            right_pos =0.99;
+            }   
+        if (left_pos <=0){
+            left_pos =0.01;
+            }
+        if (left_pos >=1.0){
+            left_pos =0.99;
+            }   
+        if (right_pos <=0){
+            right_pos =0.01;
+            }
+        leftGrip.setPosition(left_pos);
+        rightGrip.setPosition(right_pos);
+            
+        //Chain and Extend
+        ExtendPower  = gamepad2.left_stick_y ;
+        ChainPower   = gamepad2.right_stick_y ;
+    
+        Extend.setPower(ExtendPower/3);
+        Chain.setPower(ChainPower/3);
+        
+        //Drive/Pivot
+        double pivotR;
+        double verticalDrvF;
+        double verticalDrvR;
+        double verticalDrv;
+        double horizontalDrv;
+    
+        pivotR  = -gamepad1.left_stick_x ;
+        verticalDrv = -gamepad1.right_stick_y;
+        verticalDrvF = gamepad1.right_trigger;
+        horizontalDrv = gamepad1.right_stick_x;
+        verticalDrvR = gamepad1.left_trigger;
+       
+        if((gamepad1.right_trigger == 0.0) && (gamepad1.left_trigger == 0.0) && (gamepad1.right_stick_y == 0.0) && (gamepad1.right_stick_x == 0.0)){
+        Motor1.setPower(pivotR);
+        //Motor2.setPower(pivotR);
+        //Motor3.setPower(pivotR);
+        Motor4.setPower(pivotR);
+        }
+        
+        if((gamepad1.left_stick_x == 0.0) && (gamepad1.left_trigger == 0.0)&&(gamepad1.right_stick_x == 0.0)&&(gamepad1.right_stick_y == 0.0)){
+        //Motor2.setPower(verticalDrvF);
+        Motor4.setPower(-verticalDrvF);
+        Motor1.setPower(verticalDrvF);
+        //Motor3.setPower(-verticalDrvF);
+        }
+
+        if((gamepad1.left_stick_x == 0.0) && (gamepad1.right_trigger == 0.0)&&(gamepad1.right_stick_x == 0.0)&&(gamepad1.right_stick_y == 0.0)){
+        //Motor2.setPower(-verticalDrvR);
+        Motor4.setPower(verticalDrvR);
+        Motor1.setPower(-verticalDrvR);
+        //Motor3.setPower(verticalDrvR);     
+        }
+        
+        
+        if((gamepad1.left_stick_x == 0.0)&&(gamepad1.left_trigger == 0.0)&&(gamepad1.right_trigger == 0.0)){
+        //Motor2.setPower(verticalDrv);
+        Motor4.setPower(-verticalDrv);
+        Motor1.setPower(horizontalDrv);
+        //Motor3.setPower(-horizontalDrv); 
+        }
+        //End of Main
+        
+        }
+    }
+}
